fbe2070c3c6264de009d15d4fb1d7430fd26783a
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 18:17:35 +0200

    Class done.

--- src/main/java/org/lasencinas/regEx/RegularExpressions.java
+++ src/main/java/org/lasencinas/regEx/RegularExpressions.java
@@ -1,4 +1,37 @@
 package org.lasencinas.regEx;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegularExpressions {
+
+    /* ---- Properties ---- */
+    private Map<String, String> regularExpressions = null;
+
+
+    /* ---- Constructor ---- */
+    public RegularExpressions() {
+        this.regularExpressions = new HashMap<>();
+    }
+
+
+    /* ---- Getters ----- */
+    public Map<String, String> getRegularExpressions() {
+        return regularExpressions;
+    }
+
+
+    /* ---- Behaviours ---- */
+    public void addRegExpression(String identifier, String regEx) {
+        getRegularExpressions().putIfAbsent(identifier, regEx);
+    }
+
+    public String takeOutRegExpression(String identifier) {
+        String regEx = getRegularExpressions().get(identifier);
+        return regEx;
+    }
+
+    public int getSize() {
+        return getRegularExpressions().size();
+    }
 }
