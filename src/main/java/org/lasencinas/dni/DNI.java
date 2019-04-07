46dba4c6831babf0090689a00d9a1fdf2512d8eb
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 17:26:55 +0200

    Class done.

--- src/main/java/org/lasencinas/dni/DNI.java
+++ src/main/java/org/lasencinas/dni/DNI.java
@@ -1,4 +1,53 @@
 package org.lasencinas.dni;
 
 public class DNI {
+
+    /* ---- Properties ---- */
+    private int numbers = 0;
+    private Character letter = null;
+    private String dni = null;
+
+
+    /* ---- Constructor ---- */
+    public DNI() {
+
+    }
+
+    public DNI(int numbers, Character letter) {
+        this.numbers = numbers;
+        this.letter = letter;
+    }
+
+
+    /* ---- Getters  ---- */
+    public int getNumbers() {
+        return numbers;
+    }
+
+    public Character getLetter() {
+        return letter;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setLetter(Character letter) {
+        this.letter = letter;
+    }
+
+
+    /* ---- Behaviours ---- */
+    public void formDNI() {
+        String dni = "";
+        dni += getNumbers() + getLetter().toString();
+        this.dni = dni;
+
+    }
 }
