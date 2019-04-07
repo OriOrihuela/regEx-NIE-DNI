1ac8855e77c41ab562967bc53f7e4e96c5605b25
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 18:35:24 +0200

    Class done.

--- src/main/java/org/lasencinas/validator/Validator.java
+++ src/main/java/org/lasencinas/validator/Validator.java
@@ -1,4 +1,38 @@
 package org.lasencinas.validator;
 
+import org.lasencinas.dni.DNI;
+import org.lasencinas.nie.NIE;
+import org.lasencinas.regEx.RegularExpressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
+
+    /* ---- Behaviours ---- */
+    public static void validateDNI(DNI dni, RegularExpressions regularExpressions, String identifier) {
+
+        String regEx = regularExpressions.takeOutRegExpression(identifier);
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(dni.getDni());
+
+        if (matcher.matches()) {
+            System.out.println("This DNI is valid");
+        } else {
+            System.out.println("This is not a valid DNI, please enter a correct one or we will call the police");
+        }
+    }
+
+    public static void validateNIE(NIE nie, RegularExpressions regularExpressions, String identifier) {
+
+        String regEx = regularExpressions.takeOutRegExpression(identifier);
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(nie.getNie());
+
+        if (matcher.matches()) {
+            System.out.println("This NIE is valid");
+        } else {
+            System.out.println("This is not a valid NIE, please enter a correct one or we will call the police");
+        }
+    }
 }
