e49cce1baf53bf4267a92bf243f4894f48efa5fa
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 18:17:53 +0200

    Basic Test cases passed :D

---
+++ src/test/java/org/lasencinas/regEx/RegularExpressionsTest.java
@@ -0,0 +1,41 @@
+package org.lasencinas.regEx;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class RegularExpressionsTest {
+
+    /* ---- Properties ---- */
+    private RegularExpressions regularExpressions = null;
+
+
+    /* ---- Behaviours ---- */
+    @Before
+    public void __init__() {
+        regularExpressions = new RegularExpressions();
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(regularExpressions);
+    }
+
+    @Test
+    public void addRegExpressionTest() {
+        String regEx = "^\\d{8}[A-Z&&[^IÑOU]]{1}$";
+        String regEx2 = "^[XYZ]{1]\\d{7}[A-Z&&[^IÑOU]]{1]$";
+
+        regularExpressions.addRegExpression("DNI", regEx);
+        regularExpressions.addRegExpression("NIE", regEx2);
+
+        assertNotNull(regularExpressions.getRegularExpressions());
+        assertEquals(2, regularExpressions.getSize());
+        assertEquals(regEx, regularExpressions.takeOutRegExpression("DNI"));
+        assertEquals(regEx2, regularExpressions.takeOutRegExpression("NIE") );
+    }
+
+
+}
