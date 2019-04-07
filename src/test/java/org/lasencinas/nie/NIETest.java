7df2bd55470cf7cac787f7a7468b578b70bfc60b
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 17:41:25 +0200

    Basic test cases passed.

---
+++ src/test/java/org/lasencinas/nie/NIETest.java
@@ -0,0 +1,45 @@
+package org.lasencinas.nie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NIETest {
+
+    /* ---- Properties ---- */
+    private NIE nie = null;
+
+
+    /* ---- Behaviours ---- */
+    @Before
+    public void __init__() {
+        nie = new NIE('X',3204761, 'C');
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(nie);
+    }
+
+    @Test
+    public void gettersAndSettersTest() {
+        assertEquals(3204761, nie.getNumbers());
+        Character answer = 'C';
+        assertEquals(answer, nie.getFinalLetter());
+
+        nie.setFirstLetter('G');
+        Character answer1 = 'G';
+        assertEquals(answer1, nie.getFirstLetter());
+
+        nie.setNumbers(6987423);
+        assertEquals(6987423, nie.getNumbers());
+    }
+
+    @Test
+    public void formDNITest() {
+        nie.formNIE();
+        assertNotNull(nie.getNie());
+        assertEquals("X3204761C", nie.getNie());
+    }
+}
