83e7e517e15de27c53f70eaa59195494da43fab7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 17:41:14 +0200

    Class done.

--- src/main/java/org/lasencinas/nie/NIE.java
+++ src/main/java/org/lasencinas/nie/NIE.java
@@ -1,4 +1,62 @@
 package org.lasencinas.nie;
 
 public class NIE {
+
+    /* ---- Properties ---- */
+    private Character firstLetter = null;
+    private int numbers = 0;
+    private Character finalLetter = null;
+    private String nie = null;
+
+
+    /* ---- Constructor ---- */
+    public NIE() {
+
+    }
+
+    public NIE(Character firstLetter, int numbers, Character finalLetter) {
+        this.firstLetter = firstLetter;
+        this.numbers = numbers;
+        this.finalLetter = finalLetter;
+    }
+
+
+    /* ---- Getters ---- */
+    public Character getFirstLetter() {
+        return firstLetter;
+    }
+
+    public int getNumbers() {
+        return numbers;
+    }
+
+    public Character getFinalLetter() {
+        return finalLetter;
+    }
+
+    public String getNie() {
+        return nie;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setFirstLetter(Character firstLetter) {
+        this.firstLetter = firstLetter;
+    }
+
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setFinalLetter(Character finalLetter) {
+        this.finalLetter = finalLetter;
+    }
+
+
+    /* ---- Behaviours ---- */
+    public void formNIE() {
+        String nie = "";
+        nie = getFirstLetter().toString() + getNumbers() + getFinalLetter().toString();
+        this.nie = nie;
+    }
 }
