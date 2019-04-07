6ce610a666ec29956aec34d84194e1f0fd1cec20
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-07 19:52:12 +0200

    New properties and methods implemented.

--- src/main/java/org/lasencinas/validator/Validator.java
+++ src/main/java/org/lasencinas/validator/Validator.java
@@ -4,12 +4,55 @@ import org.lasencinas.dni.DNI;
 import org.lasencinas.nie.NIE;
 import org.lasencinas.regEx.RegularExpressions;
 
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
 
+    /* ---- Properties of the class ---- */
+    private Set<DNI> dniSet = null;
+    private Set<NIE> nieSet = null;
+
+
+    /* ---- Constructor ---- */
+    public Validator() {
+
+    }
+
+    public Validator(Set<DNI> dniSet, Set<NIE> nieSet) {
+        this.dniSet = dniSet;
+        this.nieSet = nieSet;
+    }
+
+
+    /* ---- Getters ---- */
+    public Set<DNI> getDniSet() {
+        return dniSet;
+    }
+
+    public Set<NIE> getNieSet() {
+        return nieSet;
+    }
+
+    public int getDniSetSize() {
+        return getDniSet().size();
+    }
+
+    public int getNieSetSize() {
+        return getNieSet().size();
+    }
+
+
     /* ---- Behaviours ---- */
+    public void addDni(DNI dni) {
+        getDniSet().add(dni);
+    }
+
+    public void addNie(NIE nie) {
+        getNieSet().add(nie);
+    }
+
     public static boolean validateDNI(DNI dni, RegularExpressions regularExpressions, String identifier) {
 
         String regEx = regularExpressions.takeOutRegExpression(identifier);
@@ -40,5 +83,41 @@ public class Validator {
                     "will call the police.");
             return false;
         }
+    }
+
+    public void formDocuments() {
+        for (DNI dni : dniSet) {
+            dni.formDNI();
+        }
+        for (NIE nie : nieSet) {
+            nie.formNIE();
+        }
+    }
+
+    public void countValidDocuments(RegularExpressions regularExpression) {
+        int correctDNIs = 0;
+        int correctNIEs = 0;
+        int incorrectDNIs = 0;
+        int incorrectNIEs = 0;
+
+        for (DNI dni : getDniSet()) {
+            if (Validator.validateDNI(dni, regularExpression, "DNI")) {
+               correctDNIs += 1;
+            } else {
+                incorrectDNIs += 1;
+            }
+        }
+        for (NIE nie : getNieSet()) {
+            if (Validator.validateNIE(nie, regularExpression, "NIE")) {
+                correctNIEs += 1;
+            } else {
+                incorrectNIEs += 1;
+            }
+        }
+
+        System.out.println("The number of correct DNIs is " + correctDNIs + '.' + '\n' +
+                "The number of incorrect DNIs is " + incorrectDNIs + '.' + '\n' +
+                "The number of correct NIEs is " + correctNIEs + '.' + '\n' +
+                "The number of incorrect NIes is " + incorrectNIEs + '.' + '\n');
     }
 }
